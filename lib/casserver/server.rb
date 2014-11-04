789ac1bdac3ed2037e01ffa63d5d8119b49bcfe4
Mauro Giannandrea <mgiannandrea@navionics.com>
2014-11-05 00:06:39 +0100

    add webstore token on cas registration

--- lib/casserver/server.rb
+++ lib/casserver/server.rb
@@ -447,7 +447,7 @@ module CASServer
             @authenticated = true
             @authenticated_username = @username
             extra_attributes.merge!(auth.extra_attributes) unless auth.extra_attributes.blank?
-            token = auth.create_token(@username, @password)
+	    token = auth.create_token(@username, @password)
             successful_authenticator = auth
             break
           end
@@ -463,9 +463,10 @@ module CASServer
           tgt = generate_ticket_granting_ticket(@username, extra_attributes)
           response.set_cookie('tgt', tgt.to_s)
           response.set_cookie("remember_token", :value => token,
-                    :domain => "navionics.com",
+		    :domain => "navionics.com",
                     :path => "/",
                     :expires => (Time.now + 1209600))
+
           $LOG.debug("Ticket granting cookie '#{tgt.inspect}' granted to #{@username.inspect}")
 
           if @service.blank?
@@ -888,7 +889,7 @@ module CASServer
       credentials_are_valid = false
       extra_attributes = {}
       successful_authenticator = nil
-
+      token = nil
       begin
         auth_index = 0
         settings.auth.each do |auth_class|
@@ -910,6 +911,7 @@ module CASServer
             @authenticated_username = @username
             extra_attributes.merge!(auth.extra_attributes) unless auth.extra_attributes.blank?
             successful_authenticator = auth
+            token = auth.create_token(@username, @password)
             break
           end
 
@@ -923,6 +925,10 @@ module CASServer
           # 3.6 (ticket-granting cookie)
           tgt = generate_ticket_granting_ticket(@username, extra_attributes)
           response.set_cookie('tgt', tgt.to_s)
+          response.set_cookie("remember_token", :value => token,
+                    :domain => "navionics.com",
+                    :path => "/",
+                    :expires => (Time.now + 1209600))
           @lt = generate_login_ticket.ticket
 
           $LOG.debug("Ticket granting cookie '#{tgt.inspect}' granted to #{@username.inspect}")
