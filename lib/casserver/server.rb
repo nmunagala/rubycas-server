5531efb92eb2e7ecf95cd3ab50c84622f19ab0cb
Mauro <mauro.giannandrea@gmail.com>
2015-02-11 15:18:51 +0100

    add domain for cors

--- lib/casserver/server.rb
+++ lib/casserver/server.rb
@@ -277,7 +277,10 @@ module CASServer
     end
 
     before do
-      headers 'Access-Control-Allow-Origin' => '*'
+      headers 'Access-Control-Allow-Origin' => "#{request.scheme}://#{request.host}"
+      headers 'Access-Control-Allow-Origin' => "#{request.scheme}://#{request.referer}"
+      headers 'Access-Control-Allow-Origin' => "#{request.scheme}://#{request.host}:#{request.port}"
+      headers 'Access-Control-Allow-Origin' => "#{request.scheme}://#{request.referer}:#{request.port}"
       headers 'Access-Control-Allow-Credentials' => 'true'
       headers 'Access-Control-Allow-Headers' => 'Authorization,Accepts,Content-Type,X-CSRF-Token,X-Requested-With'
       headers 'Access-Control-Allow-Methods' => 'GET,POST,PUT,DELETE,OPTIONS'
